582c33520eabe979d516772bb2b294e083504918
HalenDev57 <henrysu2763@gmail.com>
2026-07-22 16:44:15 -0400

    I added some stuff, like sprites, and the button that makes the timer go up
    pro, toe, type

--- till-you-die/Scripts/label.gd
+++ till-you-die/Scripts/label.gd
@@ -10,9 +10,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if isreal == true and time >= 0:
+	if isreal == true and time >= 1:
 		isreal = false
 		await get_tree().create_timer(1.0).timeout
 		time -= 1
 		text = str(time)
 		isreal = true
+
+
+func _on_texture_button_pressed() -> void:
+	time += 1
+	text = str(time)
